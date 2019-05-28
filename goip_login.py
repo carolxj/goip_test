319fbe64b6e6fe2b48ff5d12ba6ad9412f0e8a35
carolxj <13686876820@163.com>
2019-05-28 13:43:31 +0800

    send at

--- goip_login.py
+++ goip_login.py
@@ -12,6 +12,10 @@ import sys
 import io
 import urllib.request
 import http.cookiejar
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+
+
 
 if not os.path.exists("log"):
     print("log file don't exist.create")
@@ -26,7 +30,7 @@ logging.basicConfig(#filename="log/goip_recovery.log", # 指定输出的文件
 
 logging.info("#######################test start#######################")
 
-'''
+
 class CheckInfo(object):
     def __init__(self, result, count, start_time, use_time):
         self.result = result  # 判断模块是否检测完成，0-未完成，1-完成
@@ -47,16 +51,42 @@ class DevInfo(object):
 
 check = CheckInfo(0, 0, time.time(), 0)
 check.time_out = 0
-dev = DevInfo("192.168.1.57", "80", "root", "123456")
-'''
-url = "http://192.168.1.60/login_en.html"
+dev = DevInfo("192.168.1.60", "80", "root", "root123")
+
+
+def try_fun(func):
+    if callable(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            try:  # 进行异常捕获
+                response = func(*args, **kw)
+            except Exception as e:
+                print(e)
+                response = None
+            return response
+        return wrapper
+    else:
+        def decorator(fn):
+            @functools.wraps(fn)
+            def wrapper(*args, **kw):
+                try:  # 进行异常捕获
+                    response = fn(*args, **kw)
+                except Exception as e:
+                    print(e)
+                    response = None
+                return response
+            return wrapper
+        return decorator
+
+
+url = "http://" + dev.ip + "/login_en.html"
 body = {}
 headers = {'content-type':"application/x-www-form-urlencoded"}
 response = requests.post(url, headers = headers)
-#print(response.text)
-
+# print(response.text)
 cookies_nonce = re.findall('var cookies_nonce = "(.*?)";', response.text)
 print(cookies_nonce[0])
+
 
 def md5value(s):
     password = hashlib.md5()
@@ -68,48 +98,113 @@ s = "root:" + "root123:" + cookies_nonce[0]
 passwordmd5 = md5value(s)
 
 
-url = "http://192.168.1.60/login_en.html"
-encoded_value ="root:"+ passwordmd5
+url = "http://" + dev.ip + "/login_en.html"
+encoded_value = "root:" + passwordmd5
 params = {"nonce":"",
-        "encoded":encoded_value,
-        "loginStatus":""
-         }
-#http请求头
+         "encoded" : encoded_value,
+        "loginStatus" : "",
+
+     }
+# http请求头
+cookies = "username=5;" "auth_00a277=" + cookies_nonce[0]
 header = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "Cache-Control":"max-age=0",
-    "Connection":"keep-alive",
-    "Host":"192.168.1.60",
-    "Referer":"http://192.168.1.60/login_en.html",
-    "cookie":"cookies_nonce",
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "cookie": cookies,
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 }
-response = requests.post(url,data=params)
-#print(response.text)
-
+response = requests.post(url, headers = header, data=params)
+# print(response.text)
 loginStatus = re.findall('id="ID_LoginStatus" name="loginStatus" value="(.*?)"',response.text)
 print(loginStatus[0])
-'''
-#发送get请求获取post_status页面
-url = "http://192.168.1.60/port_status_en.html"
-header = {
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "Cache-Control":"max-age=0",
-    "Connection":"keep-alive",
-    "cookie": "cookies_nonce;username=5",
-    "Host":"192.168.1.60",
-    "Referer":"http://192.168.1.60/main_left_en.html",
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-}
-response = requests.get(url)
-print(response.text)
-'''
 
-def recovery_fun():
+
+# 发送get请求获取post_status页面
+url = "http://" + dev.ip + "/port_status_en.html"
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "cookie": cookies,
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+}
+response = requests.get(url, headers = header)
+# print(response.text)
+
+def goip_command_reptile():
+    requests_result = requests.get(url, headers = header, timeout=(5, 5))
+    requests_code = requests_result.status_code
+
+    if 200 == requests_code:
+
+        print("code:%d" % requests_result.status_code)
+        # print(requests_result.text)
+
+        port_status = re.findall("strPortStatus = '(.*?)'", requests_result.text)
+        # print(port_status)
+        # list转成字典
+        port_dict = json.loads(port_status[0])
+        print(port_status)
+        max_ports = port_dict["count"]
+        print(max_ports)
+
+        # 获取模块状态
+        module_status = {}
+        check_ng = 0
+        for i in range(max_ports):
+            module_status[i] = port_dict["data"][i][3]
+            print(module_status[i])
+
+            # 判断模块是否都返回，只要有一个不正常则置标志位
+            if (("" != modem_status[i])
+            and ("ati\\r\\nQuectel_Ltd\\r\\nQuectel_M26\\r\\nRevision: M26FBR03A03-TTS\\r\\nOK" != modem_status[i])
+            and ("ati\\r\\nOK" != modem_status[i])
+            and ("OK\r\nREG?" != modem_status[i])
+            and ("OK\r\nSQ" != modem_status[i])
+            and ("OK" != modem_status[i])
+            and ("AT No Response" != modem_status[i])):
+                check_ng = 1
+                logging.info("modem %d ng,result:%s" % (i + 1, modem_status[i]))
+
+        if 0 == check_ng:
+             logging.info(modem_status)
+
+             check.use_time = time.time() - check.start_time
+             logging.info("test: %d ,use time:%s" % (check.count, check.use_time))
+             check.count += 1
+
+             check.start_time = time.time()
+
+             check.result = 0
+             check.time_out = 0  # 超时清零
+
+        else:
+            logging.info("check_result ng count: %d" % check.time_out)
+            check.result = 1
+            check.time_out += 1  # 超时加1
+
+            logging.info(modem_status)
+
+            check.use_time = time.time() - check.start_time
+            logging.info("test: %d ,use time:%s" % (check.count, check.use_time))
+            check.count += 1
+
+            check.start_time = time.time()
+
+    else:
+         assert requests_code == 200  # 状态码不是200，也会报错并充实
+         print("code:%d" % requests_code)
+
+    return requests_result
+
+def send_AT_command():
     # 绑定浏览器
     Browser = webdriver.Chrome()
     # 最大化浏览器
@@ -121,20 +216,23 @@ def recovery_fun():
     # 用户名的定位
     Browser.find_element_by_id("accountID").clear()
     Browser.find_element_by_id("accountID").send_keys(dev.username)
-    time.sleep(1)
+    time.sleep(0.5)
     # 密码的定位
     Browser.find_element_by_id("passwordID").clear()
     Browser.find_element_by_id("passwordID").send_keys(dev.password)
-    time.sleep(1)
+    time.sleep(0.5)
     # 点击登录
     Browser.find_element_by_id("btnLoginID").click()
-    time.sleep(1)
+    time.sleep(0.5)
+    #点击取消重置密码
     # 判断弹出内容，并处理
     result = expected_conditions.alert_is_present()(Browser)
     if result:
         result = Browser.switch_to.alert
         print(result.text)
         if "用户名或密码错误" in result.text:
+            #点击取消
+           # Browser.find_element_by_xpath("//*[@id="modify_pswId"]/div/form/div[3]/input[2]").check()
             print("密码错误,停止测试")
             exit()
         else:
@@ -144,4 +242,50 @@ def recovery_fun():
     Browser.switch_to.frame(0)
     # 切换到左侧frame
     Browser.switch_to.frame("left")
+    # 点击网关设置
+    Browser.find_element_by_id("H3").click()
+    time.sleep(0.5)
+    # 点击AT命令
+    Browser.find_element_by_xpath('//*[@id="M3"]/ul/li[9]/a/span').click()
+    time.sleep(0.5)
+
+    # 返回主页，重新选择frame
+    Browser.switch_to.default_content()
+    Browser.switch_to.frame(0)
+    # 切换到右侧frame
+    Browser.switch_to.frame("right")
+    # 勾选所有
+    Browser.find_element_by_id("__ID_PortChkBoxesAll").click()
+    time.sleep(0.5)
+    # AT命令定位
+    Browser.find_element_by_id("ID_goip_at_cmd").clear()
+    Browser.find_element_by_id("ID_goip_at_cmd").send_keys("ati")
+    time.sleep(0.5)
+    # 执行命令
+    Browser.find_element_by_xpath('//*[@id="ID_UssdCommandBox_Body"]/table[2]/tbody/tr[2]/td[2]/input[2]').click()
+    time.sleep(1)
+    time.sleep(1)    # 点击完AT发送，先等一会儿等待消息更新
+    # 关闭浏览器
+    Browser.quit()
+
+    logging.info("send_AT_command ok")
+    check.result = 1
+
+
+while True:
+
+    if check.time_out >= 80:  # 超过20次都没有回复，退出
+        logging.info("time out stop")
+        exit()
+
+    if 0 == check.result:
+        send_AT_command()
+    else:
+        goip_command_reptile()
+
+    time.sleep(2)
+
+
+
+
 
